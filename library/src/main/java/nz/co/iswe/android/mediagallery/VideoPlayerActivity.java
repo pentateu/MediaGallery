67fa8559878c476d1e98e1ba6acd50889b169c72
Rafael Almeida <pentateu@gmail.com>
2015-11-18 20:03:21 +1300

    feat (Video Activity) add close button

--- library/src/main/java/nz/co/iswe/android/mediagallery/VideoPlayerActivity.java
+++ library/src/main/java/nz/co/iswe/android/mediagallery/VideoPlayerActivity.java
@@ -5,6 +5,7 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -36,5 +37,13 @@ public class VideoPlayerActivity extends Activity {
             }
         });
         videoView.setVideoURI(Uri.parse(url));
+
+        Button closeButton = (Button) findViewById(R.id.close_button);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
